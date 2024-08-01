81f2ee2268ab55fabef48b4cdf3b184a1dde5c6f
James Minton <james7minton@gmail.com>
2024-08-02 00:15:01 +0100

    added some more server side validation and a reservation summary page

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 	"github.com/alexedwards/scs/v2"
 	"github.com/jminton7/bookings/internal/config"
 	"github.com/jminton7/bookings/internal/handlers"
+	"github.com/jminton7/bookings/internal/models"
 	"github.com/jminton7/bookings/internal/render"
 )
 
@@ -19,6 +21,8 @@ var session *scs.SessionManager
 
 // main is the main function
 func main() {
+	//what am I going to put in the session
+	gob.Register(models.Reservation{})
 	// change this to true when in production
 	app.InProduction = false
 
