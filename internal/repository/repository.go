851393cd8b7ada42628cb8be7a0fed90b142901b
James Minton <james7minton@gmail.com>
2024-08-22 21:53:52 +0100

    created migration to fix not needed fk and added new postgres method to search all rooms for availaiblity

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -10,5 +10,6 @@ type DatabaseRepo interface {
 	AllUsers() bool
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) (error)
-	SearchAvailabilityByDates(start,end time.Time, roomId int) (bool, error) 
+	SearchAvailabilityByDatesByRoomId(start,end time.Time, roomId int) (bool, error) 
+	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error) 
 }
