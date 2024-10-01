4a58bb49e73cf143e0c41f49fe8be493e34cb92d
James Minton <james7minton@gmail.com>
2024-10-01 23:00:10 +0100

    adding some migrations

---
+++ migrations/20241001215732_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('General''s Quarters','2024-08-19 22:11:56.000','2024-08-19 22:12:03.000'),
+	 ('Major''s Suite','2024-08-19 22:11:56.000','2024-08-19 22:11:56.000');
---
+++ migrations/20241001215923_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+	 ('Reservation','2024-08-20 00:00:00.000','2024-08-20 00:00:00.000'),
+	 ('Owner Block','2024-08-22 00:00:00.000','2024-08-22 00:00:00.000');
